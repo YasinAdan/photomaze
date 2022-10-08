e9dcc76bd10f9a5df61bc690f0b76f0c46114895
Yasin <yasinadan2001@gmail.com>
2022-10-08 14:41:45 -0500

    about us page

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AboutUS from '../components/about'
+
+
+const AboutPage =  () => {
+return (
+    <>
+        <AboutUS />
+    </>
+)
+}
+
+export default AboutPage
