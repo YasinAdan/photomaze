5709f505e5fc9e76dca2b3df6d1fd2cbf34fd2cf
Yasin <yasinadan2001@gmail.com>
2022-10-10 23:22:21 -0500

    work page

--- src/pages/ourwork.jsx
+++ src/pages/ourwork.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+// image imports
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
 
 function OurWork() {
   return (
-    <div>Our Work</div>
-  )
+    <Work>
+      <Movie>
+        <h2>The Athlete</h2>
+        <div className="line"> </div>
+        <Link to="/ourwork">
+          <img src={athlete} alt="image of an athlete" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="line"> </div>
+        <Link to="/ourwork">
+          <img src={theracer} alt="image of a racer" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>Good Times</h2>
+        <div className="line"> </div>
+        <Link to="/ourwork">
+          <img src={goodtimes} alt="goodtimes" />
+        </Link>
+      </Movie>
+    </Work>
+  );
 }
 
-export default OurWork
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+const Movie = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background-color: #cccccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
+
+
+export default OurWork;
